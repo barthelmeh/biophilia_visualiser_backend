cc53c570dd84890d9d367e3051c99139bf65d54c
Tom Barthelmeh <tba105@uclive.ac.nz>
2024-07-23 21:55:03 +1200

    Implemented administrator login functionality

--- src/app/controllers/user.controller.ts
+++ src/app/controllers/user.controller.ts
@@ -1,10 +1,27 @@
 import { Request, Response } from 'express';
 import Logger from '../../config/logger';
 import * as User from '../models/user.model';
+import * as Password from '../services/password';
+import {uid} from 'rand-token';
 
 const login = async (req: Request, res: Response): Promise<void> => {
     try {
-        Logger.info("Attempting login");
+        const user = await User.getAdministratorByUsername(req.body.username);
+
+        if(user == null || !(await Password.compare(req.body.password, user.password))) {
+            res.statusMessage = `Invalid username/password`;
+            res.status(401).send();
+            return;
+        }
+
+        // Generate the login token
+        const token = uid(64);
+
+        await User.login(user.id, token);
+
+        res.status(200).send({"userId": user.id, "token": token});
+        return;
+
     } catch (err) {
         Logger.error(err);
         res.statusMessage = "Internal Server Error";
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,10 +1,38 @@
 import { getPool } from "../../config/db";
 import sql from 'mssql';
 
+const register = async (participant: ParticipantRegister): Promise<number[]> => {
+    const query = `
+        INSERT INTO Participant (FirstName, LastName, Email, Age, ActivityLevel, HasAcceptedTerms)
+        VALUES (@FirstName, @LastName, @Email, @Age, @ActivityLevel, @HasAcceptedTerms)
+    `;
+    const result = await getPool().request()
+        .input('FirstName', participant.firstName)
+        .input('LastName', participant.lastName)
+        .input('Email', participant.email)
+        .input('Age', participant.age)
+        .input('ActivityLevel', participant.activityLevel)
+        .input('HasAcceptedTerms', participant.hasAcceptedTerms)
+        .query(query);
+    return result.rowsAffected;
+}
+
+const getAdministratorByUsername = async (username: string): Promise<Administrator | null> => {
+    const query = `SELECT Id as id, Username as username, Password as password, Token as token FROM Administrator WHERE username = @username`;
+    const result = await getPool().request()
+        .input('username', username)
+        .query(query);
+
+    if(result.recordset.length > 0) {
+        return result.recordset[0] as Administrator;
+    }
+    return null;
+}
+
 // Administrator login
 const login = async (id: number, token: string): Promise<number[]> => {
     // TODO: Update query to reflect schema
-    const query = `UPDATE administrator SET auth_token = ? WHERE id = @id`;
+    const query = `UPDATE Administrator SET Token = @token WHERE Id = @id`;
     const result = await getPool().request()
         .input('id', sql.Int, id)
         .input('token', sql.NVarChar, token)
@@ -21,10 +49,27 @@ const logout = async (id: number): Promise<number[]> => {
     return result.rowsAffected;
 }
 
-const getAllParticipants = async (): Promise<number[]> => {
-    const query = `SELECT * FROM participant`;
-    const result = await getPool().request().query(query);
-    return result.rowsAffected;
-}
+// const getParticipant = async (id: number): Promise<Participant | null> => {
+//     const query = `SELECT * FROM participant WHERE id = @id`;
+//     const result = await getPool().request()
+//         .input('id', sql.Int, id)
+//         .query(query);
 
-export { login, logout, getAllParticipants }
+//     // No participant found with ID
+//     if (result.recordset.length == 0) {
+//         return null
+//     }
+
+//     const row = result.recordset[0];
+//     const participant: Participant = {
+//         id: row.id,
+//         firstName: row.first_name,
+//         lastName: row.last_name,
+//         email: row.email,
+//         age: row.age,
+
+//     }
+//     return participant;
+// }
+
+export { register, getAdministratorByUsername, login, logout }
